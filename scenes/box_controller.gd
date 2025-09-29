17499812b5666dc13c11038b84d563361e74f8e1
Kellen-N <kellenswang3@gmail.com>
2025-09-28 21:47:31 -0500

    Added Box Spawning Function
    Added a simple box functionality to the main scene.

--- scenes/box_controller.gd
+++ scenes/box_controller.gd
@@ -1,10 +1,8 @@
 extends Node2D
+
 @export var boxSpeed = 300
 
 @onready var box_spawner: Node2D = $"../BoxSpawner"
 
 func _process(delta: float):
 	position.x += boxSpeed * delta 
-	if position.x > 700:
-		position.x = box_spawner.position.x
-	
---
+++ scripts/game_screen.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var box = preload("res://scenes/boxes.tscn")
+@onready var boxSpawner : Node2D = $"BoxSpawner"
+
+func _on_timer_timeout() -> void:
+	var boxInstance = box.instantiate()
+	boxInstance.position = boxSpawner.position
+	add_child(boxInstance)
+	print("super")
+	$Timer.start()
+	
