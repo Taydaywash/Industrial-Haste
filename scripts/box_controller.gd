787d7638efb2c32879d74c6be056117281eaf7bf
Kellen-N <kellenswang3@gmail.com>
2025-10-05 21:17:16 -0500

    Added Simple Box Types

--- scripts/box_controller.gd
+++ scripts/box_controller.gd
@@ -1,12 +1,29 @@
 extends Node2D
 
-@export var boxSpeed = 200
+@export var boxSpeed = 150
 @export_enum ("Normal", "Opened", "Tapeless", "Dirty", "Mislabeled", "Bulging") var boxType: int
+@export var boxList = ["Normal", "Opened", "Tapeless", "Dirty", "Mislabeled", "Bulging"]
+@export var conditon = "fixed"
 
 @onready var box_spawner: Node2D = $"../BoxSpawner"
 
 func _ready() -> void:
-	pass
+	match boxType:
+		0:
+			$Area2D/Sprite.modulate = Color("red")
+		1:
+			$Area2D/Sprite.modulate = Color("orange")
+		2:
+			$Area2D/Sprite.modulate = Color("yellow")
+		3:
+			$Area2D/Sprite.modulate = Color("green")
+		4:
+			$Area2D/Sprite.modulate = Color("blue")
+		5:
+			$Area2D/Sprite.modulate = Color("purple")
+		_:
+			pass
+	
 
 func _process(delta: float):
 	position.x += boxSpeed * delta 
@@ -14,3 +31,7 @@ func _process(delta: float):
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	print("box deleted")
 	queue_free()
+	
+#func _change_box(number: int):
+	#boxType = boxList[number]
+	
--- scripts/gameController.gd
+++ scripts/gameController.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
-
+@onready var box_controller = $"../Boxes"
 @onready var box = preload("res://scenes/boxes.tscn")
 @onready var boxSpawner : Node2D = $"BoxSpawner"
+
+var count = 0
 
 func _on_timer_timeout() -> void:
 	var boxInstance = box.instantiate()
 	boxInstance.position = boxSpawner.position
 	add_child(boxInstance)
 	print("box spawned in")
+	count += 1
 	$Timer.start()
 	
+func print2():
+	print("Hello")
