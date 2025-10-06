c43e8d4021ab727a4b64258df9d65dbb29f00980
Taydaywash <tayvenwashington1@gmail.com>
2025-10-05 19:34:14 -0500

    Added Title Screen Buttons

--- scripts/titleScreen.gd
+++ scripts/titleScreen.gd
@@ -1,9 +1,20 @@
 extends Control
+@onready var level_select_backdrop: Panel = $LevelSelectBackdrop
+
+#Testing
+func _on_test_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/GameScreen.tscn")
 
 #Main Menu
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 	
-#Level Select
 func _on_level_select_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/GameScreen.tscn")
+	level_select_backdrop.visible = true
+
+#Level Select
+func _on_level_select_back_button_pressed():
+	level_select_backdrop.visible = false
+
+func _on_level_button_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/LevelOne.tscn")
