59a049b0ae17cd49b19966d41da4e1f4ff78bb48
Taydaywash <tayvenwashington1@gmail.com>
2025-10-13 10:30:40 -0500

    Adjusted Clock Functionality
    Changed the clock to count every 10 minute

--- scripts/gameController.gd
+++ scripts/gameController.gd
@@ -5,9 +5,23 @@ extends Node2D
 @onready var boxSpawner : Node2D = $"BoxSpawner"
 
 var count = 0
+@onready var clockText: Label = $Clock
+var gameTime = 0
+var hour = 6
+var min = 0
 
 func _second_passed():
-	pass
+	# 30 seconds is 1 minute ingame
+	gameTime += 2
+	
+	if (gameTime >= 360):
+		return
+	if (gameTime % 60 == 0):
+		hour = (6 + gameTime/60)
+	if (gameTime % 10 == 0):
+		min = ((gameTime/10) - (6 * (hour - 6)))
+		print(min)
+	clockText.text = str(hour) + ":" + str(min) + "0"
 
 func _on_timer_timeout() -> void:
 	var boxInstance = box.instantiate()
