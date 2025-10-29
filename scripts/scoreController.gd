4256aadb32d29e1b156ba4ee371e1559486c60f1
Taydaywash <tayvenwashington1@gmail.com>
2025-10-28 19:03:57 -0500

    Defaulted Score to 0

--- scripts/scoreController.gd
+++ scripts/scoreController.gd
@@ -4,11 +4,13 @@ var score = 0
 
 #Adding points to the score
 func add_points(points):
+	self.get_child(0).play("_score_gain")
 	score = score + points
 	self.text = "Score: " + str(score)
 
 #Subtracting points from the score
 func subtract_points(points):
+	self.get_child(0).play("_score_loss")
 	if score - points > 0:
 		score = score - points
 		self.text = "Score: " + str(score)
